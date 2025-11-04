31f8ce595847aeabdd945c065adc6e2a78877332
Owen Rummage <owen@rummage.cc>
2025-11-04 12:16:36 -0600

    fuck prisma 2 electric boogaloo

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,6 +1,6 @@
 import { DATABASE_URL } from '$env/static/private';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/prisma/client';
 
 const adapter = new PrismaPg({ connectionString: DATABASE_URL });
 export const prisma = new PrismaClient({ adapter });
